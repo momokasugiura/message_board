7c717cbd95a25b934cfcc036d8e78807dffa68e7
momoka.sugiura <momoka.sugiura.d088@dirbato.co.jp>
2024-04-23 12:03:16 +0900

    ページネーションを追加

--- message_board/src/models/Message.java
+++ message_board/src/models/Message.java
@@ -14,10 +14,14 @@ import javax.persistence.NamedQuery;
 
 @Entity
 @NamedQueries({
+	@NamedQuery(
+            name = "getAllMessages",
+            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            ),
     @NamedQuery(
-        name = "getAllMessages",
-        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
-    )
+            name = "getMessagesCount",
+            query = "SELECT COUNT(m) FROM Message AS m"
+            )
 })
 @Table(name = "messages")
 public class Message {
